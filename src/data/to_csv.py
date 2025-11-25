eace763197613d086b74d454b7e29866d6277ee3
EllaHanson <ellahanson.126@gmail.com>
2025-11-25 00:21:55 -0800

    blah

--- src/data/to_csv.py
+++ src/data/to_csv.py
@@ -1,23 +1,29 @@
 import pandas as pd
 import os
+import glob
 
 pd.set_option("display.max_rows", None)  # show all rows
 
 input_dir = "./dirty_data"
-output_dir = "./csv_data"
-os.makedirs(output_dir, exist_ok=True)
+output_dir = "./csv_data/snap_data.csv"
+os.makedirs("./csv_data", exist_ok=True)
 
-dir_files = glob.glob(os.path.join(input_dir, "*.xls"))
+excel_files = glob.glob(os.path.join(input_dir, "**", "*.xls*"), recursive=True)
+all_dfs = []
 
-sheet_dict = pd.read_excel(input_f, sheet_name=None, engine="xlrd")
+for file in excel_files:
+    file_name = os.path.splitext(os.path.basename(file))[0]
 
-data = sheet_dict["NERO"]
-sheets_df = []
+    if file.endswith(".xlsx"):
+        csv_dict = pd.read_excel(file, sheet_name=None, engine="openpyxl")
+    else:
+        csv_dict = pd.read_excel(file, sheet_name=None, engine="xlrd")
 
-for name, data in sheet_dict.items():
-    data = data.reset_index(drop=True)
-    data.insert(0, "SheetName", name)
-    sheets_df.append(data)
+    for sheet_name, df in csv_dict.items():
+        df = df.reset_index(drop=True)
+        df.insert(0, "SheetName", sheet_name)
+        df.insert(0, "FileName", file_name)
+        all_dfs.append(df)
 
-tot_file = pd.concat(sheets_df)
-tot_file.to_csv(output_f, index=False)
+complete_df = pd.concat(all_dfs, ignore_index=True)
+complete_df.to_csv(output_dir, index=False)
